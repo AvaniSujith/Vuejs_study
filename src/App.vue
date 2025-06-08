e695a3e306fd998f2f46bedf80975d78c6b59ce9
AvaniSujith <chinchuavani123@gmail.com>
2025-06-08 23:27:40 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import  HomeCards from './components/HomeCards.vue';
+import JobListings from './components/JobListings.vue';
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <!-- <Hero title="Test Title" subtitle="Test Subtitle"/> -->
+     <Hero />
+    <HomeCards />
+    <JobListings :limit="3" :show-button="true"/>
+  </div>
+</template>
